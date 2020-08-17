a98cb4aed9e2659710a9561dc34d65bc4bf20e44
Masoud Khairi <msdkhairi@gmail.com>
2020-08-18 00:13:51 +0430

    update some data settings

--- settings.py
+++ settings.py
@@ -6,8 +6,11 @@ from torch import Tensor
 DATA_ROOT = '/path/to/VOC'
 DATA_LIST = None
 IMG_MEAN = Tensor(np.array([0.485, 0.456, 0.406]))
-DPT_MEAN = 94.50936413431783
-# STD = Tensor(np.array([0.229, 0.224, 0.225]))
+IMG_STD = Tensor(np.array([0.229, 0.224, 0.225]))
+DPT_MEAN = 0.37
 SCALE_RANGE = (0.5, 2.0)
-CROP_SIZE = (350, 350)
+CROP_SIZE = 350
 IGNORE_LABEL = 0
+DPT_IGNORE_LABEL = -1
+BCE_IGNORE_LABEL = -2
+

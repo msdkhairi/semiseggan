74a4bb23be02c22bb7b4fc4a9c52326e3e671ed5
Masoud Khairi <msdkhairi@gmail.com>
2020-08-30 00:19:29 +0430

    Add discriminator network

---
+++ model/discriminator.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Discriminator(nn.Module):
+
+	def __init__(self, num_classes, ndf = 64):
+		super(Discriminator, self).__init__()
+
+		self.conv1 = nn.Conv2d(num_classes, ndf, kernel_size=4, stride=2, padding=1)
+		self.conv2 = nn.Conv2d(ndf, ndf*2, kernel_size=4, stride=2, padding=1)
+		self.conv3 = nn.Conv2d(ndf*2, ndf*4, kernel_size=4, stride=2, padding=1)
+		self.conv4 = nn.Conv2d(ndf*4, ndf*8, kernel_size=4, stride=2, padding=1)
+		self.classifier = nn.Conv2d(ndf*8, 1, kernel_size=4, stride=2, padding=1)
+
+		self.leaky_relu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+
+
+	def forward(self, x):
+		x = self.conv1(x)
+		x = self.leaky_relu(x)
+		x = self.conv2(x)
+		x = self.leaky_relu(x)
+		x = self.conv3(x)
+		x = self.leaky_relu(x)
+		x = self.conv4(x)
+		x = self.leaky_relu(x)
+		x = self.classifier(x) 
+        
+		return x
+
+if __name__ == "__main__":
+    num_classes = 38
+    model = Discriminator(num_classes)
+    model.eval()
+    image = torch.randn(8, num_classes, 350, 350)
+    with torch.no_grad():
+        output = model.forward(image)
+    print(output.size())
+	

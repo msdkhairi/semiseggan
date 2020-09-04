ac293cdb916f18afd71ebe1f0148ed327f5db5be
Masoud Khairi <msdkhairi@gmail.com>
2020-09-04 16:47:24 +0430

    Add evaluate function which returns all metrics

---
+++ metric.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+from sklearn.metrics import confusion_matrix
+
+import settings
+
+def evaluate(label_gt, label_pred):
+
+
+    ignore_label = settings.IGNORE_LABEL
+    num_classes = settings.NUM_CLASSES
+
+    conf_mat = confusion_matrix(label_gt, label_pred, labels=np.arange(num_classes))
+
+    # omit ignore label row and column from confusion matrix 
+    if ignore_label >= 0 and ignore_label < num_classes:
+        row_omitted = np.delete(conf_mat, ignore_label, axis=0)
+        conf_mat = np.delete(row_omitted, ignore_label, axis=1)
+
+    # calculate metrics
+    acc = np.diag(conf_mat).sum() / conf_mat.sum()
+    acc_cls = np.diag(conf_mat) / conf_mat.sum(axis=1)
+    acc_cls = np.nanmean(acc_cls)
+    iou = np.diag(conf_mat) / (conf_mat.sum(axis=1) + conf_mat.sum(axis=0) - np.diag(conf_mat))
+    mean_iou = np.nanmean(iou)
+    freq = conf_mat.sum(axis=1) / conf_mat.sum()
+    fwavacc = (freq[freq > 0] * iou[freq > 0]).sum()
+
+    return {
+        'pAcc' : acc,
+        'mAcc': acc_cls,
+        'iou' : iou,
+        'fIoU': fwavacc,
+        'mIoU': mean_iou,
+    }
+
